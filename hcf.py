8db6c76ad3de4e671d8188455f0d9149fe8fd98b
Ch.Prasanna <170031562@kluniversity.in>
2021-05-26 15:56:17 +0530

    HCF of 2 numbers

---
+++ hcf.py
@@ -0,0 +1,13 @@
+def hcf(x,y):
+    small=0
+    if(x>y):
+        small=y
+    else:
+        small=x
+    for i in range(1,small+1):
+        if((x%i==0) and (y%i==0)):
+            h=i
+    return h
+x=int(input())
+y=int(input())
+print(hcf(x,y))
